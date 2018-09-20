9062c94c56adaeb1fe732cd23e6fc6bde71fcca3
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-09-20 16:43:57 -0700

    Update data-structures-algorithms-js.js

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -120,7 +120,7 @@ var filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
 var array1 = [1, 4, 9, 16];
 
 // pass a function to map
-const map1 = array1.map(x => x * 2);// [2, 8, 18, 32]
+const map1 = array1.map(x => x * 2);	// [2, 8, 18, 32]
 
 
 // .REDUCE()
