80ffb3bb162ee9a721aae61ca6ed997429f9b411
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-09-21 14:04:09 -0700

    Update data-structures-algorithms-js.js

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -105,7 +105,7 @@ var array1 = ['a', 'b', 'c'];
 var array2 = ['d', 'e', 'f'];
 
 console.log(array1.concat(array2));
-// expected output: Array ["a", "b", "c", "d", "e", "f"]
+// ["a", "b", "c", "d", "e", "f"]
 
 // .FILTER()
 function isBigEnough(value) {
