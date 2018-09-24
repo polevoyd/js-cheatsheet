1c9a816bb2a5782c1c37c22c43f3718830ef9f39
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-09-24 13:57:22 -0700

    Update data-structures-algorithms-js.js

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -179,6 +179,11 @@ console.log(Array.from([1, 2, 3], x => x + x));
 
 
 // The Array.isArray() method determines whether the passed value is an Array.
+Array.isArray([1, 2, 3]);  // true
+Array.isArray({foo: 123}); // false
+Array.isArray('foobar');   // false
+Array.isArray(undefined);  // false
+
 
 
 
