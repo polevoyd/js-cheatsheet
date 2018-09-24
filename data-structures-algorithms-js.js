9d33d63f4d3561207b8fdc842728eccda94fc0b2
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-09-24 14:00:54 -0700

    Update data-structures-algorithms-js.js

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -205,6 +205,15 @@ console.log(array1.copyWithin(1, 3));
 
 // The entries() method returns a new Array Iterator object that contains the key/value pairs for each index in the array.
 
+var array1 = ['a', 'b', 'c'];
+
+var iterator1 = array1.entries();
+
+console.log(iterator1.next().value);
+// expected output: Array [0, "a"]
+
+console.log(iterator1.next().value);
+// expected output: Array [1, "b"]
 
 
 
