af10bb2642d08d4169b4f8ea0608b3f00ae11d68
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-09-24 13:59:39 -0700

    Update data-structures-algorithms-js.js

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -193,6 +193,16 @@ console.log(array1.concat(array2));
 e arrays. This method does not change the existing arrays, but instead returns a new array.
 
 // The copyWithin() method shallow copies part of an array to another location in the same array and returns it, without modifying its size.
+var array1 = [1, 2, 3, 4, 5];
+
+// place at position 0 the element between position 3 and 4
+console.log(array1.copyWithin(0, 3, 4));
+// expected output: Array [4, 2, 3, 4, 5]
+
+// place at position 1 the elements after position 3
+console.log(array1.copyWithin(1, 3));
+// expected output: Array [4, 4, 5, 4, 5]
+
 
 
 
