6408cf92e446aab599c9f60e2d4f15e889468931
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-09-24 14:16:33 -0700

    Update data-structures-algorithms-js.js

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -275,6 +275,15 @@ console.log(pets.includes('cat'));
 console.log(pets.includes('at'));
 // expected output: false
 
+// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+var beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+console.log(beasts.indexOf('bison'));
+// expected output: 1
+
+// start from index 2
+console.log(beasts.indexOf('bison', 2));
+// expected output: 4
 
 
 
