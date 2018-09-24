6de92217813dde7e8bd1fea9f9b1e8e4e4d6c5f7
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-09-24 14:41:12 -0700

    Update data-structures-algorithms-js.js

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -205,8 +205,7 @@ var array1 = ['a', 'b', 'c'];
 
 var iterator1 = array1.entries();
 
-console.log(iterator1.next().value);
-// expected output: Array [0, "a"]
+console.log(iterator1.next().value);	// [0, "a"]
 
 console.log(iterator1.next().value);
 // expected output: Array [1, "b"]
