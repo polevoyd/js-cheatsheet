3bad4177241561374cdc1cd3a5f7bb1a6e5c6aa0
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-09-24 14:23:17 -0700

    Update data-structures-algorithms-js.js

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -173,8 +173,7 @@ Array.from(new Set(arr));
 //The Array.from() method creates a new, shallow-copied Array instance from an array-like or iterable object.
 console.log(Array.from('foo'));		// expected output: Array ["f", "o", "o"]
 
-console.log(Array.from([1, 2, 3], x => x + x));
-// expected output: Array [2, 4, 6]
+console.log(Array.from([1, 2, 3], x => x + x));		// expected output: Array [2, 4, 6]
 
 
 // The Array.isArray() method determines whether the passed value is an Array.
