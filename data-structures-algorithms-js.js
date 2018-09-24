d01ee0c0acf84df1eb44455e3150dd536de23500
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-09-24 14:11:36 -0700

    Update data-structures-algorithms-js.js

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -216,6 +216,15 @@ console.log(iterator1.next().value);
 // expected output: Array [1, "b"]
 
 // The fill() method fills all the elements of an array from a start index to an end index with a static value. The end index is not included.
+var array1 = [1, 2, 3, 4];
+
+// fill with 0 from position 2 until position 4
+console.log(array1.fill(0, 2, 4));
+// expected output: [1, 2, 0, 0]
+
+// fill with 5 from position 1
+console.log(array1.fill(5, 1));
+// expected output: [1, 5, 5, 5]
 
 
 
