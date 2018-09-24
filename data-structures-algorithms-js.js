e93c61dd1343907beb0701a6852005000ba4e84d
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-09-24 14:12:33 -0700

    Update data-structures-algorithms-js.js

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -227,6 +227,14 @@ console.log(array1.fill(5, 1));
 // expected output: [1, 5, 5, 5]
 
 // The find() method returns the value of the first element in the array that satisfies the provided testing function. Otherwise undefined is returned.
+var array1 = [5, 12, 8, 130, 44];
+
+var found = array1.find(function(element) {
+  return element > 10;
+});
+
+console.log(found);
+// expected output: 12
 
 
 
