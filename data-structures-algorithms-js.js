425ee85eab4d440258593e3a087a7755159079fe
Dmitry <manxxeta@gmail.com>
2018-10-03 16:42:16 -0700

    in process..

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -217,7 +217,7 @@ var pets = ['cat', 'dog', 'bat'];
 
 pets.includes('cat');  // true
 
-console.log(pets.includes('at')); // false
+pets.includes('at'); // false
 
 // The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
 var beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
