adeb39979cd5e0027b32ba8f141473a067534067
Dmitry <manxxeta@gmail.com>
2018-10-03 15:58:10 -0700

    in process..

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -190,7 +190,7 @@ var found = array1.find(function(element) {
 });
 
 
-// expected output: 12
+// 12
 
 // The findIndex() method returns the index of the first element in the array that satisfies the provided testing function. Otherwise -1 is returned.
 var array1 = [5, 12, 8, 130, 44];
