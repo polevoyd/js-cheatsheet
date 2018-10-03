7e83fd9bdd8df670cc5a86ed4fbbd9ee6d1aebd4
Dmitry <manxxeta@gmail.com>
2018-10-03 15:54:10 -0700

    in process..

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -177,7 +177,7 @@ iterator1.next().value; // [1, "b"]
 var array1 = [1, 2, 3, 4];
 
 // fill with 0 from position 2 until position 4
-console.log(array1.fill(0, 2, 4));	// expected output: [1, 2, 0, 0]
+console.log(array1.fill(0, 2, 4));	// [1, 2, 0, 0]
 
 // fill with 5 from position 1
 console.log(array1.fill(5, 1));
