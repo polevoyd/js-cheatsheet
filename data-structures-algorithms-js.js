130829641b8784a22468a26647872c05b771ac05
Dmitry <manxxeta@gmail.com>
2018-10-03 13:16:14 -0700

    in process..

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -136,14 +136,13 @@ let num = parseInt('10'); // to Integer
 
 // remove duplicates from array
 arr.filter((element, index, array) => array.lastIndexOf(element) == index);
-// или es6
+// construct an new array from a set of arr - removes duplicates
 Array.from(new Set(arr));
 
 
 //The Array.from() method creates a new, shallow-copied Array instance from an array-like or iterable object.
-console.log(Array.from('foo'));		// ["f", "o", "o"]
-
-console.log(Array.from([1, 2, 3], x => x + x));		// [2, 4, 6]
+Array.from('foo');      // ["f", "o", "o"]
+Array.from([1, 2, 3], x => x + x);  // [2, 4, 6]
 
 
 // The Array.isArray() method determines whether the passed value is an Array.
