76f542adc41911e0c014b4d660df3874b371c479
Dmitry <manxxeta@gmail.com>
2018-10-05 08:09:35 -0700

    in process..editing a cheatsheet

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -30,7 +30,7 @@
 */
 
 let array = []; 
- 	
+
 // Insert head, index, tail
 let headIns =   a.unshift(value);	   
 let indexIns =  a.splice(index, 0, value);
@@ -199,20 +199,10 @@ array1.includes(2); // true
 ['ant', 'bison', 'camel', 'duck', 'bison'].indexOf('bison', 2);	// 4
 
 
+// ◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼
+//            Linked List
+// ◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼
 
-
-
-// Linked List
-// In computer science, a linked list is a linear collection of data elements, in which linear order is not given by their physical placement in memory. Instead, each element points to the next. 
-// It is a data structure consisting of a group of nodes which together represent a sequence. Under the simplest form, each node is composed of data and a reference (in other words, a link) to 
-// the next node in the sequence. This structure allows for efficient insertion or removal of elements from any position in the sequence during iteration. More complex variants add additional links, 
-// allowing efficient insertion or removal from arbitrary element references. A drawback of linked lists is that access time is linear (and difficult to pipeline). Faster access, such as random access, 
-// is not feasible. Arrays have better cache locality as compared to linked lists.
-
-// Linked List
-
-
-// Time Complexity
 // Access	Search	Insertion	Deletion
 // O(n)	O(n)	O(1)	O(1)
 // Space Complexity
