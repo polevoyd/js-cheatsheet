0ffe3010603c510a3924653db0bbec826787f5b5
Dmitry <manxxeta@gmail.com>
2018-10-05 08:12:19 -0700

    in process..editing a cheatsheet

--- data-structures-algorithms-js.js
+++ data-structures-algorithms-js.js
@@ -208,6 +208,14 @@ array1.includes(2); // true
 // Space Complexity
 // O(n)
 
+// Use for
+
+// Insertion into the middle/beginning of the list
+// Efficient sorting (pointer swap vs. copying)
+// Do not use for
+
+// Direct access
+
 class List
 {
   constructor()
@@ -370,6 +378,82 @@ class List
  * adding, accessing, and removing values without needing to know memory
  * addresses.
  */
+
+// 1.4 List std::list and std::forward_list
+
+// Time Complexity
+
+// Operation	Time Complexity
+// Insert Head	O(1)
+// Insert Index	O(n)
+// Insert Tail	O(1)
+// Remove Head	O(1)
+// Remove Index	O(n)
+// Remove Tail	O(1)
+// Find Index	O(n)
+// Find Object	O(n)
+// Example Code
+
+// std::list<int> l;
+
+// //---------------------------------
+// // General Operations
+// //---------------------------------
+
+// // Insert head, index, tail
+// l.push_front(value);                    // head
+// l.insert(l.begin() + index, value);     // index
+// l.push_back(value);                     // tail
+
+// // Access head, index, tail
+// int head = l.front();                                           // head
+// int value = std::list<int>::iterator it = l.begin() + index;    // index
+// int tail = l.back();                                            // tail
+
+// // Size
+// unsigned int size = l.size();
+
+// // Iterate
+// for(std::list<int>::iterator it = l.begin(); it != l.end(); it++) {
+//     std::cout << *it << std::endl;
+// }
+
+// // Remove head, index, tail
+// l.pop_front();                  // head
+// l.erase(l.begin() + index);     // index
+// l.pop_back();                   // tail
+
+// // Clear
+// l.clear();
+
+// //---------------------------------
+// // Container-Specific Operations
+// //---------------------------------
+
+// // Splice: Transfer elements from list to list
+// //	splice(iterator pos, list &x)
+// //  	splice(iterator pos, list &x, iterator i)
+// //  	splice(iterator pos, list &x, iterator first, iterator last)
+// l.splice(l.begin() + index, list2);
+
+// // Remove: Remove an element by value
+// l.remove(value);
+
+// // Unique: Remove duplicates
+// l.unique();
+
+// // Merge: Merge two sorted lists
+// l.merge(list2);
+
+// // Sort: Sort the list
+// l.sort();
+
+// // Reverse: Reverse the list order
+// l.reverse();
+
+
+
+
 
 
 // ◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼◼
