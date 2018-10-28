a3e9ab1c6ffcd0693552509d37d53f88c92c6883
Dmitry <38082886+polevoyd@users.noreply.github.com>
2018-10-27 17:55:19 -0700

    Update CountDuplicates.js

--- CountDuplicates.js
+++ CountDuplicates.js
@@ -51,3 +51,36 @@ const firstNotRepeatingCharacter = (s) => {
     // by compare firstIndexOf and lastIndexOf
     return [...s].filter((e, i, a) => [...s].lastIndexOf(e) === [...s].indexOf(e))[0] || '_' ;
 }
+
+
+// Note: Write a solution that only iterates over the string once
+// and uses O(1) additional memory, // since this is what you would 
+// be asked to do during a real interview.
+
+// Given a string s, find and return the first instance of a 
+// non-repeating character in it. If there is no such character, return '_'
+
+/*********************************************************/
+const firstNotRepeatingCharacter = (s) => {
+
+    // make a map of string, then
+    // iterate array of keys and return 
+    // if counter[key] equal to 1
+    // time: N, space: N
+    const counter = {};
+
+    for (let element of s){
+        if (counter[element]){
+            counter[element]++;
+        } else {
+            counter[element]=1;
+            unique_value = element;
+        }
+    }
+    
+    for (let e of Object.keys(counter))
+        if (counter[e] === 1)
+            return e;
+    
+    return '_';
+}
